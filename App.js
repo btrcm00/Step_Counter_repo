cd958361e532a08dbc88f872bde30d7f22df1a8a
minhcs9 <congminhabcdc@gmail.com>
2021-05-20 09:54:05 +0700

    link mqtt into home

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import  React, {useEffect} from 'react';
+import  React, { useState, useEffect, Component  } from 'react';
+import {Text} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer'
 import 
@@ -10,13 +11,79 @@ import
     AnalyticScreen_calo,
     AnalyticScreen_steps,
     AnalyticScreen_km,
+    HomeScreen,
   } from './Screens';
 import { View, ActivityIndicator } from 'react-native';
+import Paho from './src/paho-mqtt'
 import {DrawerContent} from './Screens/DrawerContent';
 import { AuthContext } from './components/context';
 import RootStackScreen from './Screens/RootStackScreen';
 
 const Drawer = createDrawerNavigator();
+
+var data = '0';
+var dataChange = false;
+function Mqtt() {
+  client = new Paho.Client(host = "io.adafruit.com", port = 443, clientId =  "web_" + parseInt(Math.random() * 100, 10));
+  var options = {     
+    useSSL: true,
+    userName: "Kien1120",
+    password: "aio_heNu56NaI8yoBXbqf6FlPCSlBOLl",
+    keepAliveInterval: 60,
+    onSuccess: onConnect,
+    onFailure: onFail
+  };
+  client.connect(options);
+
+  //const [Step, setStep] = useState('0'); 
+  function onConnect() {
+    console.log("Connected!");
+    client.subscribe('Kien1120/feeds/test');  
+  }
+
+  function onFail(context) {
+    console.log(context);
+    console.log("Connection failed!");
+    Alert.alert('Failure to Connect', 
+      "Unable to connect to host. Try again later.",
+      [
+        { text: "OK" }
+      ],
+    )
+  }
+  
+  function onMessageArrived(message) {
+    console.log("Message Arrived:" + message.payloadString);
+    navigation.navigate('Home11', {
+      steps: message.payloadString,
+    });
+    //setStep(message.payloadString);    //setStep_Count(message.payloadString);
+    data = message.payloadString;
+    dataChange = true;
+
+  } 
+
+  function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+      console.log("onConnectionLost:" +  responseObject.errorMessage);
+    }
+  }   
+
+  client.onMessageArrived = onMessageArrived;     
+  client.onConnectionLost = onConnectionLost; 
+
+
+
+  return (
+    <Text>react_native_mqtt:</Text>
+  )
+}
+
+
+
+
+
+
 const App=()=>{
   //const [isLoading, setIsLoading] = React.useState(true);
   //const [userToken, setUserToken] = React.useState(null);
@@ -58,10 +125,21 @@ const App=()=>{
     }
   };
   const [loginState, dispatch] = React.useReducer(loginReducer, initialLoginState);
+  const [Step, setStep] = useState('0'); 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if(dataChange){
+        dataChange = false
+        setStep(data);
+      }
+    }, 10);
+    return () => clearInterval(interval);
+  }, []);
   const authContext = React.useMemo(() => ({
     signIn: async(foundUser) => {
       /* setUserToken('congminh');
       setIsLoading(false); */
+      Mqtt()
       const userToken = String(foundUser[0].userToken);
       const userName = foundUser[0].username;
       
@@ -116,6 +194,7 @@ const App=()=>{
         { loginState.userToken !== null ? (
           <Drawer.Navigator initialRouteName="Home" drawerContent={props => <DrawerContent {...props}/>}>
             <Drawer.Screen name = "Home" component={MainTabScreen}/>
+            <Drawer.Screen name = "Home11" component={HomeScreen}/>
             <Drawer.Screen name = "Profile" component = {ProfileScreen}/>
             <Drawer.Screen name="Leaderboard" component={LeaderboardScreen}/>
             <Drawer.Screen name="Changepass" component={ChangepassScreen}/>
