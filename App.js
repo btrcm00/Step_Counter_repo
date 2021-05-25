884d135f9997a1029d948ef1fab64e325bcbcf6c
btcm31 <congminhabcd@gmail.com>
2021-05-25 16:14:22 +0700

    a

--- App.js
+++ App.js
@@ -1,139 +1,23 @@
-import  React, { useState, useEffect, Component  } from 'react';
-import {Text} from 'react-native';
+import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import {createDrawerNavigator} from '@react-navigation/drawer'
-import 
-  { 
-    ChangepassScreen, 
-    LeaderboardScreen, 
-    MainTabScreen, 
-    ProfileScreen,
-    AnalyticScreen_calo,
-    AnalyticScreen_steps,
-    AnalyticScreen_km,
-    HomeScreen,
-  } from './Screens';
-import { View, ActivityIndicator } from 'react-native';
-import Paho from './src/paho-mqtt'
-import {DrawerContent} from './Screens/DrawerContent';
-import { AuthContext } from './components/context';
-import RootStackScreen from './Screens/RootStackScreen';
-import {firebaseApp} from './Screens/firebaseconfig'
-const Drawer = createDrawerNavigator();
-
-
-const App=()=>{
-  //const [isLoading, setIsLoading] = React.useState(true);
-  //const [userToken, setUserToken] = React.useState(null);
-
-  const initialLoginState = {
-    isLoading: true,
-    userName: null,
-    userToken: null,
-  };
-  const loginReducer = (prevState, action) => {
-    switch( action.type ) {
-      case 'RETRIEVE_TOKEN': 
-        return {
-          ...prevState,
-          userToken: action.token,
-          isLoading: false,
-        };
-      case 'LOGIN': 
-        return {
-          ...prevState,
-          userName: action.id,
-          userToken: action.token,
-          isLoading: false,
-        };
-      case 'LOGOUT': 
-        return {
-          ...prevState,
-          userName: null,
-          userToken: null,
-          isLoading: false,
-        };
-      case 'REGISTER': 
-        return {
-          ...prevState,
-          userName: action.id,
-          userToken: action.token,
-          isLoading: false,
-        };
-    }
-  };
-  const [loginState, dispatch] = React.useReducer(loginReducer, initialLoginState);
-  const authContext = React.useMemo(() => ({
-    signIn: async(foundUser) => {
-      /* setUserToken('congminh');
-      setIsLoading(false); */
-      const userToken = String(foundUser[0].userToken);
-      const userName = foundUser[0].username;
-      try {
-        await AsyncStorage.setItem('userToken', userToken);
-      } catch(e) {
-        console.log(e);
-      }
-      // console.log('user token: ', userToken);
-      dispatch({ type: 'LOGIN', id: userName, token: userToken });
-    },
-    signOut: async() => {
-      /* setUserToken(null);
-      setIsLoading(false); */
-      try {
-        await AsyncStorage.removeItem('userToken');
-      } catch(e) {
-        console.log(e);
-      }
-      dispatch({ type: 'LOGOUT' });
-    },
-    signUp: () => {
-      //setUserToken('congminh');
-      //setIsLoading(false);
-    }
-  }));
-  useEffect(() => {
-    setTimeout(async() => {
-      //setIsLoading(false);
-      let userToken;
-      userToken = null;
-      try {
-        userToken = await AsyncStorage.getItem('userToken');
-      } catch(e) {
-        console.log(e);
-      }
-      // console.log('user token: ', userToken);
-      dispatch({ type: 'RETRIEVE_TOKEN', token: userToken });
-    }, 1000);
-  }, []);
-
-  if(loginState.isLoading ) {
-    return(
-      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-        <ActivityIndicator size="large"/>
-      </View>
-    );
-  }
-  return(
-    <AuthContext.Provider value = {authContext}>
-      <NavigationContainer>
-        { loginState.userToken !== null ? (
-          <Drawer.Navigator initialRouteName="Home" drawerContent={props => <DrawerContent {...props}/>}>
-            <Drawer.Screen name = "Home" component={MainTabScreen}/>
-            <Drawer.Screen name = "Home11" component={HomeScreen}/>
-            <Drawer.Screen name = "Profile" component = {ProfileScreen}/>
-            <Drawer.Screen name="Leaderboard" component={LeaderboardScreen}/>
-            <Drawer.Screen name="Changepass" component={ChangepassScreen}/>
-            <Drawer.Screen name="MainTab" component={MainTabScreen}/>
-            <Drawer.Screen name="Analytic_steps" component={AnalyticScreen_steps}/>
-            <Drawer.Screen name="Analytic_km" component={AnalyticScreen_km}/>
-            <Drawer.Screen name="Analytic_calo" component={AnalyticScreen_calo}/>
-          </Drawer.Navigator>
-        ):<RootStackScreen/>}
-      </NavigationContainer>
-    </AuthContext.Provider>
+import { createStackNavigator } from '@react-navigation/stack';
+import { ChangepassScreen, HomeScreen, RegisterScreen, LoginScreen, DashboardScreen, LeaderboardScreen, StartScreen} from './Screens'
+const Stack = createStackNavigator();
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName = "StartScreen"
+      >
+        <Stack.Screen name = "StartScreen" component = {StartScreen}/>
+        <Stack.Screen name="LoginScreen" component={LoginScreen}  />
+        <Stack.Screen name="HomeScreen" component={HomeScreen}  />
+        <Stack.Screen name="ChangepassScreen" component={ChangepassScreen}  />
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen}  />
+        <Stack.Screen name="DashboardScreen" component={DashboardScreen}  />
+        <Stack.Screen name="LeaderboardScreen" component={LeaderboardScreen}  />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 export default App;
-
-
