937c92bb9df5c6cb01f7b6630c13fd9785c21146
DyyThong <thongphdt@gmail.com>
2021-06-02 17:00:24 +0700

    testing success

--- Screens/home.js
+++ Screens/home.js
@@ -5,9 +5,10 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import {IconButton} from 'react-native-paper';
 import Paho from '../src/paho-mqtt'
 import firebase from '../components/FirebaseConfig'
-var data = '0';
+var messageArrived = "on_shake";
 var dataChange = false;
 var runApp = false;
+var count = 0;
 function mqtt_connect() {
   var client;
   client = new Paho.Client('io.adafruit.com', 443, 'web_' + parseInt(Math.random() * 100, 10));
@@ -15,8 +16,8 @@ function mqtt_connect() {
     useSSL: true,
     // userName: 'Kien1120',
     // password: 'aio_heNu56NaI8yoBXbqf6FlPCSlBOLl',
-    userName: 'CSE_BBC',
-    password: 'aio_KXfp47zegx3CthMAEj6pB0ZeKoEm',
+    userName: 'CSE_BBC1',
+    password: 'aio_lubo29QRBLOP6OoDkQ5RDsmUSukr',
     keepAliveInterval: 60,
     onSuccess: onConnect,
     onFailure: onFail
@@ -27,7 +28,7 @@ function mqtt_connect() {
 
   function onConnect() {
     console.log("Connected!");
-    client.subscribe('CSE_BCC/feeds/accelerometer');  
+    client.subscribe('CSE_BBC1/feeds/bk-iot-accelerometer');  
   }
 
   function onFail(context) {
@@ -45,9 +46,16 @@ function mqtt_connect() {
     console.log("Message Arrived:" + message.payloadString);
     //setStep(message.payloadString);    //setStep_Count(message.payloadString);
     //data = message.payloadString;
-    data = JSON.parse(message.payloadString);
-    data = data.id;
-    dataChange = true;
+    // data = JSON.parse(message.payloadString);
+    // data = data.id;
+    // dataChange = true;
+    var mes = JSON.parse(message.payloadString);
+    messageArrived = mes.data;
+    if(messageArrived != "on_shake"){
+      count+=1;
+      messageArrived = "on_shake";
+      dataChange = true;
+    }
   } 
 
   function onConnectionLost(responseObject) {
@@ -66,10 +74,10 @@ function mqtt_connect() {
 const HomeStack = createStackNavigator();
 var height = Dimensions.get('window').height;
 function HomeStackScreen({navigation,route}){
-  const [Step, setStep] = React.useState('1000'); 
+  const [Step, setStep] = React.useState('0'); 
   const user = firebase.auth().currentUser;
   const db = firebase.firestore();
-  var target = 1200;
+  var target = 10;
   db.collection('User').doc(user.uid).get().then((doc)=>{
     if (doc.exists) {
       target = doc.data().target;
@@ -87,6 +95,46 @@ function HomeStackScreen({navigation,route}){
   console.log(target)
   var width = (Step<=target)?(((Step/target)*100).toFixed(2).toString(10) + '%'):'100%';
   if(Step==target){
+
+      var client2;
+      client2 = new Paho.Client('io.adafruit.com', 443, '');
+      var options2 = {     
+        useSSL: true,
+        // userName: 'Kien1120',
+        // password: 'aio_heNu56NaI8yoBXbqf6FlPCSlBOLl',
+        userName: 'CSE_BBC',
+        password: 'aio_EgXK400rXGsfdspfNyuk4Y2JZZMR',
+        keepAliveInterval: 60,
+        onSuccess: onConnect1,
+        onFailure: onFail1
+      };
+      client2.connect(options2);
+    ``
+      //const [Step, setStep] = useState('0'); 
+    
+      function onConnect1() {
+        console.log("Connected Speaker!");
+        var speaker={
+          "id":"2",
+          "name":"SPEAKER",
+          "data":"400",
+          "unit":""
+          };
+          var json_string = JSON.stringify(speaker);
+          client2.publish('CSE_BBC/feeds/bk-iot-speaker',json_string)
+      }
+      function onFail1(context) {
+        console.log(context);
+        console.log("Connection failed!");
+        Alert.alert('Failure to Connect',
+          "Unable to connect to host. Try again later.",
+          [
+            { text: "OK" }
+          ],
+        )
+      }
+      
+
     Alert.alert(
       'Congratulation!!!','Your target is completed',
       [
@@ -102,7 +150,7 @@ function HomeStackScreen({navigation,route}){
     const interval = setInterval(() => {
       if(dataChange){
         dataChange = false
-        setStep(data);
+        setStep(count);
       }
     }, 10);
     return () => clearInterval(interval);
