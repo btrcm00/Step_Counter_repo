03462c358930e05d07f8d5956384ba51196c9227
DyyThong <thongphdt@gmail.com>
2021-06-08 17:48:36 +0700

    add leaderboard

--- Screens/home.js
+++ Screens/home.js
@@ -14,10 +14,10 @@ function mqtt_connect() {
   client = new Paho.Client('io.adafruit.com', 443, 'web_' + parseInt(Math.random() * 100, 10));
   var options = {     
     useSSL: true,
-    // userName: 'Kien1120',
-    // password: 'aio_heNu56NaI8yoBXbqf6FlPCSlBOLl',
-    userName: 'CSE_BBC1',
-    password: 'aio_lubo29QRBLOP6OoDkQ5RDsmUSukr',
+    userName: 'Kien1120',
+    password: 'aio_heNu56NaI8yoBXbqf6FlPCSlBOLl',
+    // userName: 'CSE_BBC1',
+    // password: 'aio_lubo29QRBLOP6OoDkQ5RDsmUSukr',
     keepAliveInterval: 60,
     onSuccess: onConnect,
     onFailure: onFail
@@ -28,8 +28,9 @@ function mqtt_connect() {
 
   function onConnect() {
     console.log("Connected!");
-    client.subscribe('CSE_BBC1/feeds/bk-iot-accelerometer');  
-  }
+    // client.subscribe('CSE_BBC1/feeds/bk-iot-accelerometer');  
+    client.subscribe('Kien1120/feeds/test');
+    }
 
   function onFail(context) {
     console.log(context);
@@ -44,6 +45,7 @@ function mqtt_connect() {
   
   function onMessageArrived(message) {
     console.log("Message Arrived:" + message.payloadString);
+
     //setStep(message.payloadString);    //setStep_Count(message.payloadString);
     //data = message.payloadString;
     // data = JSON.parse(message.payloadString);
@@ -56,6 +58,20 @@ function mqtt_connect() {
       messageArrived = "on_shake";
       dataChange = true;
     }
+    const user = firebase.auth().currentUser;
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    var time = today.toLocaleTimeString();
+
+    today = dd + '-' + mm + '-' + yyyy;
+    String(today);
+    firebase.firestore().collection('User').doc(user.uid).collection('Step').doc(today).collection('Log').doc(time).set({
+      Time: time + ' ' + today,
+      Message: messageArrived, 
+      Step: count
+    })
   } 
   //count --> stepofday
   function onConnectionLost(responseObject) {
@@ -84,6 +100,10 @@ function getTime(uid,Step1){
   firebase.firestore().collection('User').doc(uid).collection('Step').doc(today).set({
     Step: Step1,
   })
+  // firebase.firestore().collection('User').doc(uid).collection('Step').doc(today).collection('Log').doc().set({
+  //   Time: time + ' ' + today, 
+  //   Step: Step1
+  // })
 }
 
 
