75b5766fb8a2374f032775d481beb071d000fde0
DyyThong <thongphdt@gmail.com>
2021-06-08 17:48:36 +0700

    add leaderboard

--- Screens/leaderboard.js
+++ Screens/leaderboard.js
@@ -3,20 +3,56 @@ import {StyleSheet,View,Image,Text, ImageBackground } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Leaderboard from 'react-native-leaderboard';
+import firebase from '../components/FirebaseConfig';
+
 const HomeStack = createStackNavigator();
 function LeaderboardStack({navigation}){
-    const state = {
-        data: [
-            {userName: 'Joe', highScore: 52},
-            {userName: 'Jenny', highScore: 120},
-            {userName: 'Thong', highScore: 33},
-            {userName: 'TH', highScore: 333},
-            //...
-        ] //can also be an object of objects!: data: {a:{}, b:{}}
-    }
+    const user = firebase.auth().currentUser;
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    var time = today.toLocaleTimeString();
+
+    today = dd + '-' + mm + '-' + yyyy;
+    String(today);
+    const db = firebase.firestore();
+    // var [Step, setStep] = React.useState('');
+    // var [Name, setName] = React.useState('');
+    // db.collection('User').doc(user.uid).collection('Step').doc('07-06-2021').onSnapshot((doc)=>{
+    //     if (doc.exists) {
+    //         //setStep(doc.data().Step);
+    //         setName(doc.data().name);
+    //         console.log(Name);
+    //     } else {
+    //         // doc.data() will be undefined in this case
+    //         console.log("No such document!");
+    //     }
+    // })
+
+    // const state = {
+    //     data: [
+    //         {userName: 'Joe', highScore: 52},
+    //         {userName: 'Jenny', highScore: 120},
+    //         {userName: 'Thong', highScore: 33},
+    //         {userName: 'TH', highScore: 333},
+    //         //...
+    //     ] //can also be an object of objects!: data: {a:{}, b:{}}
+    // }
+    var [data1, setData] = React.useState([]);
+    const unsubscribe = firebase.firestore().collection("User").get().then((snap) =>{
+        const datas = [];
+        snap.forEach((doc) =>{
+            datas.push({userName: doc.data().name, highScore: doc.data().stepsOfday})
+        })
+        setData(datas);
+    //or you can use useState like const [data, setData] = useState([]); then setData(datas)
+     })
+
+    console.log(data1);
     return(
         <Leaderboard 
-        data={state.data} 
+        data={data1} 
         sortBy='highScore' 
         labelBy='userName'/>
     );
