ea9d86f1462eb25b7ffa11c71864a91cf2ed0820
minhcs9 <congminhabcdc@gmail.com>
2021-06-09 21:27:26 +0700

    history ver2

--- Screens/history.js
+++ Screens/history.js
@@ -10,11 +10,18 @@ function HistoryStack(){
     var [data1, setData] = React.useState([]);
     db.collection("User").doc(user.uid).collection("Step").onSnapshot((snap) =>{
         const datas = [];
-        snap.forEach((doc) =>{
-            console.log(doc.data())
-            datas.push({step: doc.data().Step, time: doc.data().Time})
+        snap.forEach( doc =>{
+                doc.ref.collection("Log").get().then(snapLog=>{
+                    snapLog.forEach( aa=>{
+                        datas.push({
+                            message: aa.data().Message,
+                            step: aa.data().Step,
+                            time: aa.data().Time,
+                        })
+                    })
+                    setData(datas);
+                })
         })
-        setData(datas);
      })
     return(
         <View>
@@ -22,8 +29,7 @@ function HistoryStack(){
                 {
                     data1.map((item, index) => (
                      <View key = {item.time} style = {styles.item}>
-                        <Text>{item.step}</Text>
-                        <Text>{item.time}</Text>
+                        <Text>{item.message + " " + item.time + " with "+String(item.step) +" steps"}</Text>
                      </View>
                   ))
                }
@@ -70,7 +76,7 @@ const styles = StyleSheet.create({
     margin:5,
     borderRadius: 10,
     shadowColor: "black",
-    padding:40,
+    padding:25,
     shadowOffset: {
     width: -5,
     height: 6,
